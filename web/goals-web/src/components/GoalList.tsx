ad80f2d4102597e75a5a51b9218ae77790578102
Jonathan Belloit <Jonathan@topdogwebdev.com>
2025-06-29 06:02:39 -0400

    Started GoalList component to see how codex does at finishing it

---
+++ web/goals-web/src/components/GoalList.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import { getDocs } from 'firebase/firestore';
+
+const GoalList = () => {
+
+  const [goals, setGoals] = useState([""]);
+
+  return (
+    <div>GoalList</div>
+  )
+}
+
+export default GoalList
